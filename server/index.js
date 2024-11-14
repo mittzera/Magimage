39d7cc96351979fa40b14665ae3752f7f999f8ee
Hugo Rodrigues <hugocavalcantesr@gmail.com>
2024-11-14 02:20:56 -0300

    feat: magimage integration

--- server/index.js
+++ server/index.js
@@ -3,34 +3,31 @@ import * as dotenv from 'dotenv';
 import cors from 'cors';
 
 import connectDB from './mongodb/connect.js';
-import postRoutes from './routes/postRoutes.js'
-import magimageRoutes from './routes/magimageRoutes.js'
-
+import postRoutes from './routes/postRoutes.js';
+import magimageRoutes from './routes/magimageRoutes.js';
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(express.json({ limit:'50mb' }));
+app.use(express.json({ limit: '50mb' }));
 
-app.use('api/v1/post', postRoutes);
-app.use('api/v1/magimage', magimage);
+app.use('/api/v1/post', postRoutes);
+app.use('/api/v1/magimage', magimageRoutes);
 
-app.get('/', async(req, res) => {
-  res.send('Hello from Magimage');
-})
+app.get('/', async (req, res) => {
+  res.status(200).json({
+    message: 'Hello from magimage!',
+  });
+});
 
-const startServer = async() =>{
-
-  try{
-    connectDB(process.env.MONGODB_URL)
-    app.listen(8080, () => console.log
-      ('Server has started on port http://localhost:8080'))
-  } catch(error){
+const startServer = async () => {
+  try {
+    connectDB(process.env.MONGODB_URL);
+    app.listen(8080, () => console.log('Server started on port 8080'));
+  } catch (error) {
     console.log(error);
   }
-
-}
-
+};
 
 startServer();
